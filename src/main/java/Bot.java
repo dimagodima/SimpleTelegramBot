079b19f10edb861c5970d6925b2b6c64a1f58ad7
dimagodima <dgdwow@yandex.ru>
2019-01-18 19:13:42 +0300

    adding files

---
+++ src/main/java/Bot.java
@@ -0,0 +1,86 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bot extends TelegramLongPollingBot {
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(  );
+        try{
+            telegramBotsApi.registerBot(new Bot());
+
+
+        }catch (TelegramApiException e){
+            e.printStackTrace(  );
+        }
+    }
+
+    public void sendMsg(Message message,String text){
+        SendMessage sendMessage = new SendMessage(  );
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        try{
+            setButtons(sendMessage);
+            sendMessage(sendMessage);
+        }catch (TelegramApiException e){
+            e.printStackTrace(  );
+        }
+    }
+
+    public void onUpdateReceived(Update update) {
+        Model model = new Model();
+        Message message = update.getMessage();
+        if(message !=null && message.hasText()){
+            switch (message.getText()){
+                case"/help":
+                    sendMsg(message,"can I help you?");
+                            break;
+                case "/setting":
+                    sendMsg(message,"what will customize?");
+                    break;
+                default:
+                    try{
+                        sendMsg(message,Weather.getWeather( message.getText(),model ) );
+                    } catch (IOException e) {
+                        sendMsg( message,"City Not Found." );
+                    }
+            }
+        }
+    }
+
+    public void setButtons(SendMessage sendMessage){
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(true);
+        List<KeyboardRow> keyboardRowList = new ArrayList<>();
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        keyboardFirstRow.add(new KeyboardButton("/help") );
+        keyboardFirstRow.add(new KeyboardButton("/setting") );
+        keyboardRowList.add(keyboardFirstRow);
+        replyKeyboardMarkup.setKeyboard( keyboardRowList );
+
+    }
+
+    public String getBotUsername() {
+        return "TestGodBot";
+    }
+
+    public String getBotToken() {
+        return "752915820:AAHktpKZSxIQOsk0isqP3638OmIlFYZTDko";
+    }
+}
